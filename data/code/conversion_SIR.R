72e528a52ab7c8b61f22fef9012349bade4f68c4
valperez <valperez1214@gmail.com>
2021-03-09 19:37:28 -0600

    Con los acumulados arreglados y los chequeos

--- data/code/conversion_SIR.R
+++ data/code/conversion_SIR.R
@@ -58,7 +58,7 @@ inegi_dats <- read_excel("~/GitHub/EstrategiaVacunacionMX/data/raw/Base_datos_Po
   select(-Age) %>%
   left_join(municipios_escenarios, by = c("Estado","Municipio")) %>%
   group_by(EDAD_CAT, Estado, Municipio, Tercil, cve_ent, cve_mun) %>%
-  summarise(Pop = sum(Pop)) %>%
+  summarise(Pop = sum(Pop, na.rm = T)) %>%
   drop_na(Tercil)
 
 fecha_min <- ymd("2020/04/10")
@@ -71,7 +71,6 @@ edades <- c("Edad < 40", "Edad 40 - 49", "Edad 50 - 59",
 
 combinaciones <- crossing(data.frame(Fecha = secuencia_dias)) %>%
   crossing(data.frame(Tercil = c(1:3))) %>%
-  crossing(data.frame(ENTIDAD_FEDERATIVA = diccionario.covid$ENTIDAD_FEDERATIVA)) %>%
   crossing(data.frame(EDAD_CAT = edades)) 
 
 
@@ -119,88 +118,177 @@ dats <- dats %>%
 
 dats <- dats %>% 
   right_join(diccionario.covid, by = c("ENTIDAD_RES" = "CLAVE_ENTIDAD")) %>%
-  right_join(municipios_escenarios, by = c("ENTIDAD_RES" = "cve_ent", 
+  right_join(inegi_dats, by = c("ENTIDAD_RES" = "cve_ent", "EDAD_CAT",
                                            "MUNICIPIO_RES" = "cve_mun")) %>%
   select(-Estado, -ABREVIATURA)
 
 infectados <- dats %>%
   rename(Fecha = FECHA_SINTOMAS) %>%
   filter(Fecha > fecha_min) %>%
-  group_by(ENTIDAD_FEDERATIVA, EDAD_CAT, Fecha, Tercil, ENTIDAD_RES) %>%
+  group_by(EDAD_CAT, Fecha, Tercil) %>%
   tally() %>%
-  full_join(combinaciones, by = c("Fecha","EDAD_CAT", "Tercil", "ENTIDAD_FEDERATIVA")) %>%
+  full_join(combinaciones, by = c("Fecha","EDAD_CAT", "Tercil")) %>%
   rename(Infectados = n)
+
+# -- infectados checked sí funcionan
 
 muertos <- dats %>%
   rename(Fecha = FECHA_DEF) %>%
   filter(Fecha > fecha_min) %>%
-  group_by(ENTIDAD_FEDERATIVA, EDAD_CAT, Fecha, Tercil, ENTIDAD_RES) %>%
+  group_by(EDAD_CAT, Fecha, Tercil, ENTIDAD_RES) %>%
   tally() %>%
-  full_join(combinaciones, by = c("Fecha","EDAD_CAT", "Tercil", "ENTIDAD_FEDERATIVA")) %>%
+  full_join(combinaciones, by = c("Fecha","EDAD_CAT", "Tercil")) %>%
   rename(Muertos = n)
+
+# --- muertos checked sí funciona
 
 hospital <- dats %>%
   rename(Fecha = FECHA_INGRESO) %>%
   filter(Fecha > fecha_min) %>%
-  group_by(ENTIDAD_FEDERATIVA, EDAD_CAT, Fecha, Tercil, ENTIDAD_RES) %>%
+  group_by(EDAD_CAT, Fecha, Tercil, ENTIDAD_RES) %>%
   tally() %>%
-  full_join(combinaciones, by = c("Fecha","EDAD_CAT", "Tercil", "ENTIDAD_FEDERATIVA")) %>%
+  full_join(combinaciones, by = c("Fecha","EDAD_CAT", "Tercil")) %>%
   rename(Hospitalizados = n)
 
+# --- hospital checked sí funciona
+
+
 base_modelo <- infectados %>%
-  left_join(muertos, by = c("Fecha","EDAD_CAT", "Tercil", "ENTIDAD_FEDERATIVA", "ENTIDAD_RES")) %>%
-  left_join(hospital, by = c("Fecha","EDAD_CAT", "Tercil", "ENTIDAD_FEDERATIVA", "ENTIDAD_RES")) %>%
+  left_join(muertos, by = c("Fecha","EDAD_CAT", "Tercil")) %>%
+  left_join(hospital, by = c("Fecha","EDAD_CAT", "Tercil")) %>%
   mutate(Muertos = replace_na(Muertos, 0)) %>%
   mutate(Hospitalizados = replace_na(Hospitalizados, 0)) %>%
   mutate(Fecha = ymd(Fecha)) %>%
   filter(Fecha > ymd("2020/04/10"))
 
+
 #FIX ME el pop?
 base_modelo_final <- base_modelo %>%
   mutate(Dia = as.numeric(Fecha - !!fecha_min))  
+# --- Hasta aquí parece que igual, todo bien
 
 municipio_tercil_pop <- inegi_dats %>%
-  group_by(cve_ent, EDAD_CAT, Tercil) %>%
-  summarise(Poblacion = sum(Pop)) 
+  group_by(EDAD_CAT, Tercil) %>%
+  summarise(Poblacion = sum(Pop, na.rm = T))
+
 
 all_terciles <- data.frame(Tercil = c(1:3)) %>%
-  crossing(data.frame(cve_ent = municipio_tercil_pop$cve_ent)) %>%
   crossing(data.frame(EDAD_CAT = edades)) 
 
 not_terciles <- all_terciles %>% anti_join(municipio_tercil_pop, 
-                                           by = c("EDAD_CAT", "cve_ent", "Tercil"))
+                                           by = c("EDAD_CAT", "Tercil"))
 
-Poptotal <- inegi_dats %>% ungroup() %>% select(Pop) %>% summarise(Poblacion = sum(Pop, na.rm = T)) %>% as.numeric()
+Poptotal <- inegi_dats %>% ungroup() %>% select(Pop) %>% 
+  summarise(Poblacion = sum(Pop, na.rm = T)) %>% as.numeric()
+
 
 base_modelo_final2 <- base_modelo_final %>% 
-  left_join(municipio_tercil_pop, by = c("EDAD_CAT","Tercil","ENTIDAD_RES" = "cve_ent")) %>%
-  group_by(Fecha, Dia, Tercil, EDAD_CAT) %>% 
+  left_join(municipio_tercil_pop, by = c("EDAD_CAT","Tercil")) %>%
+  group_by(Fecha, Dia, Tercil, EDAD_CAT, Poblacion) %>% 
   summarise(Infectados     = sum(Infectados, na.rm = T), 
             Hospitalizados = sum(Hospitalizados, na.rm = T), 
-            Muertos        = sum(Muertos, na.rm = T),
-            Poblacion      = sum(Poblacion, na.rm = T)) %>%
-  mutate(I = Infectados/Poblacion)  %>%
-  mutate(H = Hospitalizados/Poblacion) %>%
-  mutate(M = Muertos/Poblacion) %>%
+            Muertos        = sum(Muertos, na.rm = T)) 
+
+base_modelo_final2 <- base_modelo_final2 %>%
+  mutate(Infectados     = replace_na(Infectados,0)) %>%
+  mutate(Hospitalizados = replace_na(Hospitalizados,0)) %>%
+  mutate(Muertos        = replace_na(Muertos,0)) %>%
+  group_by(Fecha, Dia, Tercil, EDAD_CAT) %>% 
+  mutate(I = Infectados/!!Poptotal)  %>%
+  mutate(H = Hospitalizados/!!Poptotal) %>%
+  mutate(M = Muertos/!!Poptotal) %>%
+  ungroup() %>%
+  group_by(Tercil, EDAD_CAT) %>% 
   mutate(Acumulados_I = cumsum(I)) %>%
   mutate(Acumulados_H = cumsum(H)) %>%
   mutate(Acumulados_M = cumsum(M)) %>%
-  mutate(S = Poblacion/!!Poptotal - Acumulados_I - Acumulados_H - Acumulados_M)
+  mutate(S = Poblacion/!!Poptotal - I - H - M)
+  #mutate(S = Poblacion/!!Poptotal - Acumulados_I - Acumulados_H - Acumulados_M)
 
 base_modelo_final2 <- base_modelo_final2 %>%
-  select(-Infectados, -Hospitalizados, -Muertos, -M, -Acumulados_I, -Acumulados_H) %>%
-  pivot_wider(names_from = c(EDAD_CAT, Tercil), values_from = c(S,I,H,Acumulados_M, Poblacion))
+  arrange(-Tercil) %>%
+  select(-Infectados, -Hospitalizados, -Muertos, -M) %>%
+  rename(M = Acumulados_M) %>%
+  pivot_wider(names_from = c(EDAD_CAT, Tercil), values_from = c(S,I,H,M, Acumulados_I, Acumulados_H, Poblacion))
+
+base_modelo_final3 <- 
+  base_modelo_final2 %>% 
+  select(Fecha, Dia, 
+         starts_with("S_Edad < 40"),
+         starts_with("S_Edad 40"), starts_with("S_Edad 50"),
+         starts_with("S_Edad 60"), starts_with("S_Edad 70"), 
+         starts_with("S_Edad 80"), starts_with("I_Edad < 40"),
+         starts_with("I_Edad 40"), starts_with("I_Edad 50"),
+         starts_with("I_Edad 60"), starts_with("I_Edad 70"), 
+         starts_with("I_Edad 80"), starts_with("H_Edad < 40"),
+         starts_with("H_Edad 40"), starts_with("H_Edad 50"),
+         starts_with("H_Edad 60"), starts_with("H_Edad 70"), 
+         starts_with("H_Edad 80"), starts_with("M_Edad < 40"),
+         starts_with("M_Edad 40"), starts_with("M_Edad 50"),
+         starts_with("M_Edad 60"), starts_with("M_Edad 70"), 
+         starts_with("M_Edad 80"), starts_with("Acumulados_I_Edad < 40"),
+         starts_with("Acumulados_I_Edad 40"), starts_with("Acumulados_I_Edad 50"),
+         starts_with("Acumulados_I_Edad 60"), starts_with("Acumulados_I_Edad 70"), 
+         starts_with("Acumulados_I_Edad 80"), starts_with("Acumulados_H_Edad < 40"),
+         starts_with("Acumulados_H_Edad 40"), starts_with("Acumulados_H_Edad 50"),
+         starts_with("Acumulados_H_Edad 60"), starts_with("Acumulados_H_Edad 70"), 
+         starts_with("Acumulados_H_Edad 80") )
+
+write_rds(base_modelo_final3, "datos_SIR_porTercil_julia.rds")
+
+
+# Checks para base_modelo_final3
+
+# Primero vamos a hacer un for que cheque que los acumulados todos sean mayor o 
+# igual que el anterior
+
+#para checar que sea creciente
+prueba <- base_modelo_final3 %>%
+  select(starts_with("Acumulados"))
+
+suma <- c()
+suma_par <- 0
+
+for (j in 1:ncol(prueba)){
+  for (i in 1:(nrow(prueba) - 1)){
+    suma_par <- suma_par + (prueba[i + 1, j] - prueba[i, j] < 0)
+  }
+  suma[j] <- suma_par
+}
+
+#Este resultado debe dar TRUE
+sum(suma) == 0
+
+base_positivos <- base_modelo_final3 %>%
+  select(-Fecha, -Dia)
+
+positivos <- 0
+
+#Para checar que sea positiva
+for (j in 1:ncol(base_positivos)){
+  for (i in 1:nrow(base_positivos)){
+    positivos <- positivos + (base_positivos[i,j] < 0 )
+  }
+}
+
+#Esto debe dar TRUE
+positivos == 0
+
+
+suma_1 <- base_modelo_final3 %>%
+  select(-starts_with("Acumulados"), -Fecha, -Dia)
+
+unidad <- rowSums(suma_1[1, ])
+
+#Esto debe dar TRUE
+unidad == 1
+
+
+ggplot(base_modelo_final2) +
+  geom_point(aes(x = Dia, y =`Acumulados_H_Edad < 40_1` + `Acumulados_H_Edad < 40_2` + `Acumulados_H_Edad < 40_3`)) 
 
 
 
 
 
-
-
-base_modelo_final <- left_join(inegi_dats, base_modelo,
-                    by = c("Estado" = "ENTIDAD_FEDERATIVA" , 
-                           "EDAD_CAT" = "EDAD_CAT", "Tercil" = "Tercil"))
-
-
-saveRDS(base_modelo_final, "datos_SIR_porTercil.rds")
 
